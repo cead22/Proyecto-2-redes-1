d5824554128023fd8b79790cfa08ae18768c5025
Marion <Mundos Graves@.(none)>
2010-02-19 17:24:43 -0400

    ya manda los msj recibidos por consola

--- fotos.java
+++ fotos.java
@@ -8,44 +8,63 @@ public class fotos{
     String mensaje;
     int puerto;
     String maquina;
+    String comando;
+    
+    /* Constructor */
     fotos(int port,String maq){
 	puerto = port;
 	maquina = maq;
+
     }
-    void run()
+
+    public void run()
     {
-	try{
-	    //1. creating a socket to connect to the server
-	    SocketCliente = new Socket(maquina, puerto);
-	    System.out.println("Conectado a: " +  maquina + "a traves del puerto: " + puerto );
-	    //2. get Input and Output streams
-	    salida = new ObjectOutputStream(SocketCliente.getOutputStream());
+       	try{
+	    // Se crea un socket 
+	    SocketCliente = new Socket(maquina,39141);
+	    mensaje = null;
+	    salida = new ObjectOutputStream(SocketCliente.getOutputStream()); 
 	    salida.flush();
-	    entrada = new ObjectInputStream(SocketCliente.getInputStream());
-	    //3: Communicating with the server
-	    do{
+	    do {
 		try{
-		    mensaje = (String)entrada.readObject();
-		    System.out.println("server>" + mensaje);
-		    sendMessage("Hi my server");
-		    mensaje = "bye";
+		    /* Se obtiene el comando a ejecutar */
+		    BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+		    mensaje = br.readLine();
 		    sendMessage(mensaje);
 		}
-		catch(ClassNotFoundException classNot){
-		    System.err.println("data received in unknown format");
+		catch(Exception e){
+		    e.printStackTrace();
 		}
-	    }while(!mensaje.equals("bye"));
+	    } while (!mensaje.equalsIgnoreCase("q"));
+	    
+
+	    //2. get Input and Output streams
+	   
+	   
+	    //entrada = new ObjectInputStream(SocketCliente.getInputStream());
+	    //3: Communicating with the server
+		//	    do{
+		//try{
+		//  mensaje = (String)entrada.readObject();
+		//  System.out.println("server>" + mensaje);
+		//  sendMessage("Hi my server");
+		//  mensaje = "bye";
+		//  sendMessage(mensaje);
+		//}
+		//catch(ClassNotFoundException classNot){
+		//    System.err.println("data received in unknown format");
+		//}
+		//}while(!mensaje.equals("bye"));
 	}
 	catch(UnknownHostException unknownHost){
-	    System.err.println("You are trying to connect to an unknown host!");
+	    System.err.println("Se esta tratado de conectar a un servidor desconocido, verifiquelo!");
 	}
 	catch(IOException ioException){
 	    ioException.printStackTrace();
 	}
 	finally{
-	    //4: Closing connection
+	    /* Se cierra la conexion */
 	    try{
-		entrada.close();
 		salida.close();
 		SocketCliente.close();
 	    }
@@ -54,19 +73,21 @@ public class fotos{
 	    }
 	}
     }
+
     void sendMessage(String msg)
     {
 	try{
 	    salida.writeObject(msg);
 	    salida.flush();
-	    System.out.println("client>" + msg);
 	}
 	catch(IOException ioException){
 	    ioException.printStackTrace();
 	}
+	
     }
-    public static void main(String args[])
-    {
+
+
+    public static void main(String args[]){
 	int puerto = 0;
 	String maq = null;
 	
@@ -75,23 +96,21 @@ public class fotos{
 	    System.out.println("Uso: fotos -s <servidor> -p <puerto>");
 	    System.exit(-1);
 	}
-	if (args[0] == "-s"  && args[2] == "-p") {
-	    try {
-		puerto = Integer.valueOf(args[1]);
-	    }
-	    catch(NumberFormatException e) {
-		System.out.println("El puerto debe ser un entero entre 1025 y 65536");
-	    }
-	    maq = args[3];
-	}
-	else if (args[0] == "-p" && args[2] == "-s") {
+
+	if (args[0].equals("-s")  && args[2].equals("-p")) {
 	    try {
 		puerto = Integer.valueOf(args[3]);
-	    }
-	    catch(NumberFormatException e) {
-		System.out.println("El puerto debe ser un entero entre 1025 y 65536");
+	    }catch(NumberFormatException e){
+		System.out.println("El puerto debe ser un numero entero entre 1025 y 65535");
 	    }
 	    maq = args[1];
+	}
+	else if (args[0].equals("-p") && args[2].equals("-s")) {
+	    try {
+		puerto = Integer.valueOf(args[1]);
+	    }catch(NumberFormatException e){
+		System.out.println("El puerto debe ser un numero entero entre 1025 y 65535");
+	    }maq = args[3];
 	}
 	else {
 	    System.out.println("Uso: edolab -f <maquinas> -p <puertoRemote>\n");
@@ -99,5 +118,6 @@ public class fotos{
 	}
 	fotos client = new fotos(puerto,maq);
 	client.run();
+	
     }
 }
